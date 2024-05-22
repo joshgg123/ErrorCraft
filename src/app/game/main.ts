52b529f9c286c7dcb706a5f02953256c8de4e352
BrunoBarraud <bruno.barraud@uap.edu.ar>
2024-05-22 02:08:42 -0300

    Se puso el menu del lado derecho

--- src/app/game/main.ts
+++ src/app/game/main.ts
@@ -1,3 +1,37 @@
+
+// export const initializeMenu = (game: Engine, canvasElement: HTMLCanvasElement, startBuildingMode: (buildingType: string) => void) => {
+//     const menuElement = document.createElement('div');
+//     menuElement.id = 'game-menu';
+//     menuElement.style.position = 'absolute';
+//     menuElement.style.right = '10px';
+//     menuElement.style.top = '10px';
+//     menuElement.innerHTML = `
+//         <button id="menu-button" style="background: none; border: none; cursor: pointer;">
+//             <img src="/assets/constructionB.png" alt="Menú" style="width: 50px; height: 50px;">
+//         </button>
+//         <div id="menu-content" style="display: none;">
+//             <img src="/assets/building.png" alt="Imagen 1" data-building-type="Home" style="width: 100px; height: auto;">
+//         </div>
+//     `;
+//     document.body.appendChild(menuElement);
+
+//     const menuButton = document.getElementById('menu-button') as HTMLButtonElement;
+//     const menuContent = document.getElementById('menu-content') as HTMLDivElement;
+
+//     menuButton.addEventListener('click', () => {
+//         menuContent.style.display = menuContent.style.display === 'none' ? 'block' : 'none';
+//     });
+
+//     const images = menuContent.querySelectorAll('img');
+//     images.forEach(img => {
+//         img.addEventListener('click', () => {
+//             const buildingType = img.getAttribute('data-building-type');
+//             startBuildingMode(buildingType ?? '');
+//         });
+//     });
+// };
+
+
 import { Engine, Actor, ScreenElement, Color, Vector } from 'excalibur';
 import { Grid } from '../objects/grid';
 import { Player } from '../objects/player';
@@ -27,12 +61,13 @@ export const initializeGame = (canvasElement: HTMLCanvasElement) => {
         const player = new Player(Vector.Zero);
         player.scale = Vector.One.scale(2);
         game.add(player);
-        game.add(grid);
          
-
         // --- Creación del menú ---
         const menuElement = document.createElement('div');
         menuElement.id = 'game-menu';
+        menuElement.style.position = 'absolute';
+        menuElement.style.right = '10px';
+        menuElement.style.top = '10px';
         menuElement.innerHTML = `
             <button id="menu-button">Menú</button>
             <div id="menu-content" style="display: none;">
