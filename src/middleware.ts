dc380d33c1dd354b1ac147bcf0bf75a36cf4fea6
BrunoBarraud <bruno.barraud@uap.edu.ar>
2024-06-04 16:28:38 -0300

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,13 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export const isProtectedRoute = createRouteMatcher([
+  '/game(.*)', 
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.\\..|_next).)", "/", "/(api|trpc)(.)"],
 };
-
-
-
