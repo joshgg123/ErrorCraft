f1e7f938a20ed1a786d6f8141386b4c69fc3a72b
joshgg123 <joshuagabriel12345@gmail.com>
2024-06-04 14:05:50 -0300

    fix

--- src/app/firebase/page.tsx
+++ src/app/firebase/page.tsx
@@ -26,36 +26,6 @@ const auth = getAuth(app);
 export {db};
 const analytics = getAnalytics(app);
 
-// Remove this if you do not have Firestore set up
-// for your Firebase app
-const getFirestoreData = async () => {
-  const docRef = doc(db, "example", "example-document");
-  const docSnap = await getDoc(docRef);
-  if (docSnap.exists()) {
-    console.log("Document data:", docSnap.data());
-  } else {
-    // docSnap.data() will be undefined in this case
-    console.log("No such document!");
-  }
-};
 
-export default function FirebaseUI() {
-  const { getToken } = useAuth();
-  const signInWithClerk = async () => {
-    console.log("Sign in with clerk");
-    const token = await getToken({ template: "integration_firebase" });
-    const userCredentials = await signInWithCustomToken(auth, token || "");
-     // The userCredentials.user object can call the methods of
-     // the Firebase platform as an authenticated user.
-    console.log("User:", userCredentials.user);
-  };
-  
 
-  return (
-    <main style={{ display: "flex", flexDirection: "column", rowGap: "1rem" }}>
-      <button onClick={signInWithClerk}>Sign in</button>
-      <button onClick={getFirestoreData}>Get document</button>
-    </main>
-  );
-}
 
