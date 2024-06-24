224eac59647630b7c1d98bade45921a5d7d03dd5
BrunoBarraud <bruno.barraud@uap.edu.ar>
2024-06-24 05:40:00 -0300

    fix

--- src/app/componentes/FunctionPanel.tsx
+++ src/app/componentes/FunctionPanel.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 const FunctionPanel = () => {
   return (
     <div className="functions-panel">
-      <h2>Funciones Adicionales</h2>
-      {/* Aquí puedes agregar cualquier funcionalidad adicional que necesites */}
+
     </div>
   );
 };
